4634b8c5fd8de6dc0b7773309665ac970153e3a9
PaulEdouard Boudier <paul-edouard.boudier@laposte.net>
2017-11-04 09:04:23 +0100

    semantic added for the slider and selector changed to be more precise

--- javascript/script.js
+++ javascript/script.js
@@ -7,48 +7,48 @@ $(document).ready(function() {
   if (localStorage.getItem("imageNumber") == null) {
     localStorage.setItem("imageNumber", "1");
     var number = localStorage.getItem("imageNumber");
-    $("img[data-number!=" + number + "]").addClass("hidden");
-    $("img[data-number=" + number + "]").removeClass("hidden");
+    $("img.sliderImage[data-number!=" + number + "]").addClass("hidden");
+    $("img.sliderImage[data-number=" + number + "]").removeClass("hidden");
   }
   else {
     var number = localStorage.getItem("imageNumber");
-    $("img[data-number !=" + number + "]").addClass("hidden");
-    $("img[data-number =" + number + "]").removeClass("hidden");
+    $("img.sliderImage[data-number!="+ number + "]").addClass("hidden");
+    $("img.sliderImage[data-number="+ number + "]").removeClass("hidden");
   }
 
   if (localStorage.getItem("dotState") == null) {
     localStorage.setItem("dotState", "1");
     var number = localStorage.getItem("dotState");
-    $("div[data-dot != "+ number +"]").addClass("white").removeClass("black");
-    $("div[data-dot = "+ number +"]").addClass("black").removeClass("white");
+    $("div .sliderDot[data-dot!="+ number +"]").addClass("white").removeClass("black");
+    $("div .sliderDot[data-dot="+ number +"]").addClass("black").removeClass("white");
   }
   else {
     var number = localStorage.getItem("dotState");
-    $("div[data-dot != "+ number +"]").addClass("white").removeClass("black");
-    $("div[data-dot = "+ number +"]").addClass("black").removeClass("white");
+    $("div .sliderDot[data-dot!="+ number +"]").addClass("white").removeClass("black");
+    $("div .sliderDot[data-dot="+ number +"]").addClass("black").removeClass("white");
   }
 
   $("button[data-button='left'], button[data-button='right']").on("click", function(){
     var direction = $(this).data("button");
     if (direction == "left") {
-      var length = $("img[data-number]").length; // length of array = number of images in the index.html so when i do not
+      var length = $("img.sliderImage[data-number]").length; // length of array = number of images in the index.html so when i do not
       var imageNumber = parseInt(localStorage.getItem("imageNumber")) - 1; //  match something ging leftward, i go to the opposite of the images
       localStorage.setItem("imageNumber", imageNumber.toString());
       var dotNumber = parseInt(localStorage.getItem("dotState")) - 1;
       localStorage.setItem("dotState", dotNumber.toString());
-      if ($("img[data-number = " + imageNumber + "]").length == 0) {
-        $("img[data-number = " + length + "]").removeClass("hidden");
-        $("img[data-number != " + length + "]").addClass("hidden");
-        $("div[data-dot != " + length + "]").removeClass("black").addClass("white");
-        $("div[data-dot = " + length + "]").removeClass("white").addClass("black");
+      if ($("img.sliderImage[data-number=" + imageNumber + "]").length == 0) {
+        $("img.sliderImage[data-number=" + length + "]").removeClass("hidden");
+        $("img.sliderImage[data-number!=" + length + "]").addClass("hidden");
+        $("div .sliderDot[data-dot!=" + length + "]").removeClass("black").addClass("white");
+        $("div .sliderDot[data-dot=" + length + "]").removeClass("white").addClass("black");
         localStorage.setItem("imageNumber", length);
         localStorage.setItem("dotState", length);
       }
       else {
-        $("img[data-number != " + imageNumber + "]").addClass("hidden");
-        $("img[data-number = " + imageNumber + "]").removeClass("hidden");
-        $("div[data-dot != " + dotNumber + "]").removeClass("black").addClass("white");
-        $("div[data-dot = " + dotNumber + "]").removeClass("white").addClass("black");
+        $("img.sliderImage[data-number!=" + imageNumber + "]").addClass("hidden");
+        $("img.sliderImage[data-number=" + imageNumber + "]").removeClass("hidden");
+        $("div .sliderDot[data-dot!=" + dotNumber + "]").removeClass("black").addClass("white");
+        $("div .sliderDot[data-dot=" + dotNumber + "]").removeClass("white").addClass("black");
       }
     }
     else {
@@ -56,30 +56,30 @@ $(document).ready(function() {
       localStorage.setItem("imageNumber", imageNumber.toString());
       var dotNumber = parseInt(localStorage.getItem("dotState")) + 1;
       localStorage.setItem("dotState", dotNumber.toString());
-      if ($("img[data-number = " + imageNumber + "]").length == 0) {
-        $("img[data-number = '1']").removeClass("hidden");
-        $("img[data-number != '1']").addClass("hidden");
-        $("div[data-dot != '1']").removeClass("black").addClass("white");
-        $("div[data-dot = '1']").removeClass("white").addClass("black");
+      if ($("img.sliderImage[data-number=" + imageNumber + "]").length == 0) {
+        $("img.sliderImage[data-number='1']").removeClass("hidden");
+        $("img.sliderImage[data-number!='1']").addClass("hidden");
+        $("div .sliderDot[data-dot!='1']").removeClass("black").addClass("white");
+        $("div .sliderDot[data-dot='1']").removeClass("white").addClass("black");
         localStorage.setItem("imageNumber", "1");
         localStorage.setItem("dotState", "1");
       }
       else {
-        $("img[data-number != " + imageNumber + "]").addClass("hidden");
-        $("img[data-number = " + imageNumber + "]").removeClass("hidden");
-        $("div[data-dot != " + dotNumber + "]").removeClass("black").addClass("white");
-        $("div[data-dot = " + dotNumber + "]").removeClass("white").addClass("black");
+        $("img.sliderImage[data-number!=" + imageNumber + "]").addClass("hidden");
+        $("img.sliderImage[data-number=" + imageNumber + "]").removeClass("hidden");
+        $("div .sliderDot[data-dot!=" + dotNumber + "]").removeClass("black").addClass("white");
+        $("div .sliderDot[data-dot=" + dotNumber + "]").removeClass("white").addClass("black");
       };
     }
   });
 
-  $("div[data-dot]").on("click", function(){
+  $("div .sliderDot[data-dot]").on("click", function(){
     var number = $(this).data("dot");
-    $("img[data-number = "+ number +" ]").removeClass("hidden");
-    $("img[data-number != "+ number +"]").addClass("hidden");
+    $("img.sliderImage[data-number="+ number +" ]").removeClass("hidden");
+    $("img.sliderImage[data-number!="+ number +"]").addClass("hidden");
     localStorage.setItem("imageNumber", number);
-    $("div[data-dot != "+ number +"]").removeClass("black").addClass("white");
-    $("div[data-dot = "+ number +"]").removeClass("white").addClass("black");
+    $("div .sliderDot[data-dot!="+ number +"]").removeClass("black").addClass("white");
+    $("div .sliderDot[data-dot="+ number +"]").removeClass("white").addClass("black");
     localStorage.setItem("dotState", number);
   });
 });
